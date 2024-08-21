e5c911474dada56f9b7833f027688b39ee144aa0
saurabhankam7 <saurabhankam111@gmail.com>
2024-08-21 19:53:33 +0530

    comm

--- Sortalgo/selectionpra.java
+++ Sortalgo/selectionpra.java
@@ -1,16 +1,18 @@
 package Sortalgo;
 
-public class Bubble {
+public class selectionpra {
     public static void main(String[] args) {
         int array1[] = { 55, 70, 44, 828, 10, 20 };
         for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array1.length-i-1; j++) {
-                if (array1[j]>array1[j+1]) {
-                    int temp= array1[j];
-                    array1[j]=array1[j+1];
-                    array1[j+1]=temp;
+            int smallest=i;
+            for (int j = i+1; j < array1.length; j++) {
+                if (array1[j]<array1[smallest]) {
+                    smallest=j;
                 }
             }
+            int temp= array1[smallest];
+            array1[smallest]=array1[i];
+            array1[i]=temp;
         }
         for (int i = 0; i < array1.length; i++) {
             System.out.println(array1[i]);
