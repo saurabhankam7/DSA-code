20a13734bf565a2b53fbac8c7cd350c9762bc897
saurabhankam7 <saurabhankam111@gmail.com>
2024-08-23 19:11:13 +0530

    commited

--- Queue/Queue1.java
+++ Queue/Queue1.java
@@ -1,55 +1,61 @@
 package Queue;
 
-import java.util.Queue;
 
-public class Queue1 {
-    /**
-     * Node
-     */
-    public class Node {
+/**
+ * queue1
+ */
+public class queue1 {
+    public class  Node {
     
         int data;
         Node next;
         Node(int data){
-            this.data=data;
-            this.next=null;
+            this.data= data;
+            this.next= null;
+        }
+    }
+    public class QueueOp{
+        static Node Head=null;
+        static Node Tail=null;
+        public boolean isEmpty(){
+            return Head==null && Tail==null;
+        }
+        public void Enqueue(int data){
+            Node addNode= new Node(data);
+            if (isEmpty()) {
+                Head=Tail=addNode;
+                return;
+            }
+            Tail.next= addNode;
+            Tail= addNode;
+        }
+        public int DeQueue(){
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return 0;
+            }
+            int data=Head.data;
+            Head= Head.next;
+            if (Head==null) {
+                Tail=null;
+            }
+            return data;
+        }
+        public int peek(){
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return 0;
+            }
+            return Head.data;
         }
     }
 
-    /**
-     * queueq1
-     */
-    public class queueq1 {
-    
-        Node head;
-        int rear=-1;
-        public boolean isempty(){
-            if(rear==-1){
-                return -1;
-            }
-        }
-        public void enquey(int data){
-            Node addnode= new Node(data);
-            if (isempty()) {
-                head=addnode;
-                rear++;
-            }
-            Node CurrentNode=head;
-            while (CurrentNode.next !=null) {
-                CurrentNode=CurrentNode.next;
-            }
-            CurrentNode.next=addnode;
-            rear++;
-        }
-        public void dequeue(){
-            if (isempty()) {
-                System.out.println("sorry queue is empty");
-                return;
-            }
-            head=head.next;
-        }
-    }
+
     public static void main(String[] args) {
-        
+        queue1 outer = new queue1();
+        QueueOp Q1= outer.new QueueOp();
+        Q1.Enqueue(11);
+        Q1.Enqueue(20);
+        Q1.peek();
     }
 }
